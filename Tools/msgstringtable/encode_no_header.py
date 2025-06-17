2e134b8aaf64d87b02926f142ea58f7ff2be5b1e
Kanin Temsrisuk <kaifa2910@msn.com>
2025-06-17 11:10:17 +0700

    msgstringtable.csv was functional now.

--- Tools/msgstringtable/encode_no_header.py
+++ Tools/msgstringtable/encode_no_header.py
@@ -1,7 +1,7 @@
 import csv
 import base64
 
-with open('msgstringtable.csv', 'w', newline='') as outfile, open('msgstringtable_decoded.txt', newline='') as decoded:
+with open('msgstringtable_no_header.csv', 'w', newline='') as outfile, open('msgstringtable_no_header.txt', newline='') as decoded:
     rdr = csv.reader(decoded, delimiter=',')
     for row in rdr:
         msg = base64.b64encode(row[0].encode('utf-8')).decode('utf-8')
